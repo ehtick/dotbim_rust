79da7b416ef9afd0e41a0c7507a0ff3bb0d6fbb3
paireks <w.radaczynski@gmail.com>
2024-07-28 20:38:56 +0200

    Add read_dotbim_to_value

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use uuid::Uuid;
+use serde_json;
+use std::fs;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+/// Reads dotbim file from path and creates a serde_json::Value with it
+pub fn read_dotbim_to_value(path: &str) -> serde_json::Value {
+    let text = fs::read_to_string(path).expect("Cannot read a dotbim file to string. Please check if path is correct.");
+    let json: serde_json::Value = serde_json::from_str(&text).expect("Cannot parse a dotbim file into a json. Please check if json structure is correct.");
+    json
 }
 
 #[cfg(test)]
@@ -11,6 +17,14 @@ mod tests {
     #[test]
     fn guid_works() {
         let id = Uuid::new_v4();
+
         println!("{}", id);
+    }
+
+    #[test]
+    fn test_read_dotbim_to_value() {
+        let path = "./test_files/CubesWithFaceColorsAndWithout_WithFixedNormals.bim";
+        let value = read_dotbim_to_value(path);
+        println!("{}", value);
     }
 }
